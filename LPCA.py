0a20489912dd546ac1771c537e255586e7d176cb
Alex Katopodis <aak61@duke.edu>
2023-07-05 14:48:30 -0400

    Wrote fit and transform

--- LPCA.py
+++ LPCA.py
@@ -1,11 +1,13 @@
 import numpy as np
+import scipy
 
 class LogisticPCA():
-    def __init__(self):
-        pass
+    def __init__(self, m, k):
+        self.m = m
+        self.k = k
 
     
-    def fit(self, X, m, k, tol):
+    def fit(self, X, tol, maxiters=100):
         """
         Fits the Logistic PCA model
 
@@ -16,31 +18,61 @@ class LogisticPCA():
         - tol (float): Converge criteria. Minimum allowed difference between trained model and perfect fit
         """
 
+        # Parameter Initializations
         # n: # of observations, p: # of features
-        n, d = X.shape
+        n, d = X.shape  
 
-        # Create Q, which is simply X but -1 or 1
+        # Natural parameters of the saturated model Theta_S
         Q = (2*X) - 1
+        Theta_S = self.m * Q
 
-        # Intialize the natural parameters of the saturated model Theta_S
-        Theta_S = m * Q
+        # Initialize U to the k right singular values of Q
+        U = np.linalg.svd(X)[2].T[:, :self.k]
 
-        # Initialize mu to mean(Theta_S_j) where Theta_S_j is the jth column of the saturated model
-        mu = np.mean(Theta_S, axis=0).reshape(-1, 1).T
-
-        # Initialize U to right singular values of Q
-        U = np.linalg.svd(X)[2].T
-
-        # Intialize loss
+        # Theta is the projected, then restored version of Theta
         Theta = self.project(Theta_S, U)
-        loss = self.likelihood(X, Theta)
-        print(loss)
+
+        # Initialize likelihood
+        likelihood = -1e10
+
+        for iter in range(maxiters):
+            # Update Z
+            Z = Theta + 4*(X - self.sigmoid(Theta))
+
+            # Update mu
+            mu = (1/n) * (Z - (Theta_S @ U @ U.T)).T @ np.ones((n, 1))
+
+            # Compute E and update U
+            Mu = np.ones((n, 1)) @ mu.T
+            argmax = (Theta_S - Mu).T @ (Z - Mu) + (Z - Mu).T @ (Theta_S - Mu) - (Theta_S - Mu).T @ (Theta_S - Mu)
+            eigenvectors = scipy.linalg.eig(argmax)[1]
+            U = eigenvectors[:, :self.k]
+            print(iter)
+
+            # Update Theta
+            Theta = Mu + ((Theta_S - Mu) @ U @ U.T)
+
+            # Converge criteria
+            if abs(likelihood - self.likelihood(X, Theta)) < tol:
+                break
+            else:
+                likelihood = self.likelihood(X, Theta)
+
+        self.mu = mu
+        self.U = U
+        print(U)
+
+    
+    def transform(self, X):
+        Q = (2*X) - 1
+        Theta_S = (self.m * Q) - (np.ones((X.shape[0], 1)) @ self.mu.T)
+        return Theta_S @ self.U
 
 
     def project(self, Theta_S, U):
         n = Theta_S.shape[0]
         
-        # Initialize mu to mean(Theta_S_j) where Theta_S_j is the jth column of the saturated model
+        # mean(Theta_S_j) where Theta_S_j is the jth column of the saturated model
         mu = np.mean(Theta_S, axis=0).reshape(-1, 1).T
 
         # Mu matrix is an NxD matrix where each column is the column mean of the data
