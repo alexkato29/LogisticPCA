23c9d0fbd28c9f500d3ffa22562e9f00f70413ba
Alex Katopodis <aak61@duke.edu>
2023-07-06 11:51:59 -0400

    Optimizations

--- LPCA.py
+++ LPCA.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy
+import time
 
 class LogisticPCA():
     def __init__(self, m, k):
@@ -7,7 +8,7 @@ class LogisticPCA():
         self.k = k
 
     
-    def fit(self, X, tol, maxiters=100):
+    def fit(self, X, tol, maxiters=1000):
         """
         Fits the Logistic PCA model
 
@@ -27,35 +28,46 @@ class LogisticPCA():
         Theta_S = self.m * Q
 
         # Initialize U to the k right singular values of Q
-        U = np.linalg.svd(X)[2].T[:, :self.k]
+        U = np.linalg.svd(Q)[2].T[:, :self.k]
 
-        # Theta is the projected, then restored version of Theta
-        Theta = self.project(Theta_S, U)
+        # Initialize mu to logit(X_bar)
+        mu = np.mean(X, axis=0).reshape(-1, 1).T
+        mu = self.logit(mu)
 
         # Initialize likelihood
-        likelihood = -1e10
+        likelihood = -100
 
         for iter in range(maxiters):
-            # Update Z
-            Z = Theta + 4*(X - self.sigmoid(Theta))
-
-            # Update mu
-            mu = (1/n) * (Z - (Theta_S @ U @ U.T)).T @ np.ones((n, 1))
-
-            # Compute E and update U
-            Mu = np.ones((n, 1)) @ mu.T
-            argmax = (Theta_S - Mu).T @ (Z - Mu) + (Z - Mu).T @ (Theta_S - Mu) - (Theta_S - Mu).T @ (Theta_S - Mu)
-            eigenvectors = scipy.linalg.eig(argmax)[1]
-            U = eigenvectors[:, :self.k]
+            # Create a matrix Mu
+            Mu = np.ones((n, 1)) @ mu
 
             # Update Theta
             Theta = Mu + ((Theta_S - Mu) @ U @ U.T)
 
+            # Update Z
+            Z = Theta + 4*(X - self.sigmoid(Theta))
+
+            # Update mu
+            mu = (1/n) * ((Z - (Theta_S @ U @ U.T)).T @ np.ones((n, 1))).T
+
+            # Compute E and update U
+            Theta_centered = Theta_S - Mu
+            Z_centered = Z - Mu
+            argmax = (Theta_centered.T @ Z_centered) + (Z_centered.T @ Theta_centered) - (Theta_centered.T @ Theta_centered)
+            print(self.is_symmetric(argmax))
+            eigenvectors = scipy.linalg.eig(argmax)[1]
+            U = eigenvectors[:, :self.k]
+
             # Converge criteria
-            if abs(likelihood - self.likelihood(X, Theta)) < tol:
+            new_likelihood = self.likelihood(X, Theta)
+            if  new_likelihood - likelihood < tol:
+                print("Reached Convergence on Iteration #" + str(iter + 1))
+                break
+            elif likelihood > new_likelihood:
+                print("Likelihood decreased, this should never happen. There is likely a bug.")
                 break
             else:
-                likelihood = self.likelihood(X, Theta)
+                likelihood = new_likelihood
 
         self.mu = mu
         self.U = U
@@ -72,20 +84,8 @@ class LogisticPCA():
         - Theta (matrix): Mean centered projection of the natural parameters
         """
         Q = (2*X) - 1
-        Theta_S = (self.m * Q) - (np.ones((X.shape[0], 1)) @ self.mu.T)
+        Theta_S = (self.m * Q) - (np.ones((X.shape[0], 1)) @ self.mu)
         return Theta_S @ self.U
-
-
-    def project(self, Theta_S, U):
-        n = Theta_S.shape[0]
-        
-        # mean(Theta_S_j) where Theta_S_j is the jth column of the saturated model
-        mu = np.mean(Theta_S, axis=0).reshape(-1, 1).T
-
-        # Mu matrix is an NxD matrix where each column is the column mean of the data
-        Mu = (np.ones((n, 1)) @ mu)
-
-        return Mu + ((Theta_S - Mu) @ U @ U.T)
 
         
     def likelihood(self, X, Theta):
@@ -96,17 +96,7 @@ class LogisticPCA():
         - X (matrix): Original binary data
         - Theta (matrix): Estimated natural parameters
         """
-        n, d = X.shape
-        P = self.sigmoid(Theta)
-
-        tr = np.trace(X.T @ Theta)
-        
-        s = 0
-        for i in range(n):
-            for j in range (d):
-                s += np.log(1 - P[i][j])
-
-        return tr + s
+        return np.sum(X * Theta - np.log(1 + np.exp(Theta)))
     
 
     def sigmoid(self, X):
@@ -119,5 +109,17 @@ class LogisticPCA():
         Returns:
         - A (matrix): Matrix with sigmoid funciton applied elementwise
         """
-        t = np.exp(-1 * X)
-        return np.reciprocal(1 + t)
+        clipped_X = np.clip(X, -1 * self.m, self.m)
+        t = np.exp(-clipped_X)
+        return 1.0 / (1.0 + t)
+    
+
+    def logit(self, x):
+        logit = np.log(x / (1 - x))
+        clipped = np.clip(logit, -1 * self.m, self.m)
+        return clipped
+    
+
+    def is_symmetric(self, X):
+        transpose = np.transpose(X)
+        return np.array_equal(X, transpose)
